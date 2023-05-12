ce4e6c46ea5effdebb9adf1eb03eeba4f87a6667
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-05-12 16:21:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 let input = document.getElementById("evaluatedText")
 let h3 = document.getElementById("letterCount")
 
-input.addEventListner('keyup',counting);
-function counting(){
-
-h3.innerText=input.value.length;
+function countChar(){
+	h3.innerText = input.value.length
 }
+input.addEventListener("keyup", countChar)
 
