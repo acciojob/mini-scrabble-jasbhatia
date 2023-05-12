32764fd6be13774689276cbbe0cdf198ed0c20f6
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-05-12 16:14:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let text=document.getElementById('eveluatedText').value;
+text.addEventListner('onChange',counting);
+function counting(){
+let count= text.match(/\s/g);
+	document.getElementById('letterCount').innerText=count;
+}
+
